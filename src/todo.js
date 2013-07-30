8148537e0caeca4f5743d8ef52255c4ac639d914
Wesley Carr <surfswell17@gmail.com>
2013-07-29 20:13:56 -0400

    Organizing and working towards adding new tasks

--- src/todo.js
+++ src/todo.js
@@ -1,36 +1,36 @@
-var todoApp = {}
+var todoApp = {
+  // Create tasks under the duplicate (clone) function
+  newTodo: function(task) {
+    var todo = duplicate(todoItem);
+    todo.task = task;
+    return task;
+  }
+  // DOM MANIPULATION - Monkey Bars
+  addThisTask: function (task) {
+    // add task from input by applying todoApp.newTodo function
+    var taskInput = document.getElementById('new-task-field').value;
+    var theNewTask = todoApp.newTodo(taskInput);
+    var todoList = document.getElementById('todo-items');
+    var todoItems = document.getElementById('new-todo');
+    // append li (child) text to ul parent.
+    todoItems.innerText = addThisTask.task;
+    todoList.appendChild(todoItems);
+  }
+};
 var todoItem = {}
-
-// Object duplicating (creating / cloning)
-function duplicate(todoItem) {
-  var duplicate = function() {};
-  duplicate.prototype = todoItem;
-  return new duplicate();
-}
-// Create tasks under the duplicate (clone) function
-todoApp.newTodo = function(task) {
-  var todo = duplicate(todoItem);
-  todo.task = task;
-  return task;
-}
-
-// DOM MANIPULATION - Monkey Bars
-var addThisTask = function () {
-  // add task from input by applying todoApp.newTodo function
-  var taskInput = document.getElementById('new-task-field').value;
-  var theNewTask = todoApp.newTodo(taskInput);
-  var todoList = document.getElementById('todo-items');
-  var todoItems = document.getElementById('new-todo');
-  // append li (child) text to ul parent.
-  todoItems.innerText = newTodo.task;
-  todoList.appendChild(todoItems);
-}
-
 
 window.onload = function () {
   //click event to pass input into a new todo object
   var addTaskButton = document.getElementById('add-item');
   addTaskButton.onclick = function (event) {
-    new
+    addThisTask();
   }
+}
+
+// Object duplicating (creating / cloning)
+// GAWD FUNCTION
+function duplicate(todoItem) {
+  var duplicate = function() {};
+  duplicate.prototype = todoItem;
+  return new duplicate();
 }
